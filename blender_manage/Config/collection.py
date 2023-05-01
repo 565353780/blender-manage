86b6daaa457f12e1dcb8d2b15a5369f09414cf91
chLi <565353780@qq.com>
2023-05-01 19:56:34 +0800

    update collection, add render name list

--- blender_manage/Config/collection.py
+++ blender_manage/Config/collection.py
@@ -2,5 +2,13 @@
 # -*- coding: utf-8 -*-
 
 COLLECTION_NAME_LIST = [
-    'GT', 'ISR+M', 'VPP-S2C+M', 'Ours+M', 'ISR+A', 'VPP-S2C+A', 'Ours'
+    'GT',
+    'ISR+M',
+    'VPP-S2C+M',
+    'Ours+M',
+    'ISR+A',
+    'VPP-S2C+A',
+    'Ours',
 ]
+
+RENDER_NAME_LIST = COLLECTION_NAME_LIST
