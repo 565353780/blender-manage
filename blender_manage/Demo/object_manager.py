8c4e1960167dc743d7e7f253eb686c57294be54d
chLi <565353780@qq.com>
2023-05-01 18:05:37 +0800

    finish object manager module

--- blender_manage/Demo/object_manager.py
+++ blender_manage/Demo/object_manager.py
@@ -1,6 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from blender_manage.Module.object_manager import ObjectManager
+
 
 def demo():
     object_manager = ObjectManager()
-    object_list = object_manager.getObjects()
+
+    collection_name_list = object_manager.getCollectionNameList()
+    print('collection_name_list:')
+    print(collection_name_list)
+
+    if len(collection_name_list) > 0:
+        collection_object_name_list = object_manager.getCollectionObjectNameList(
+            collection_name_list[0])
+        print('collection[' + collection_name_list[0] + '] object_name_list:')
+        print(collection_object_name_list)
     return True
--- blender_manage/Module/object_manager.py
+++ blender_manage/Module/object_manager.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import bpy
 
 
@@ -5,15 +8,34 @@ class ObjectManager(object):
     def __init__(self):
         return
 
-    def getObjects(self):
-        objects = bpy.data.objects
-        #  for obj in objects:
-        #  print(obj.name)
+    def getObjectList(self):
+        return bpy.data.objects
 
-        collections = bpy.data.collections
-        for collection in collections:
-            for obj in collection.objects:
-                print(collection.name, '-->', obj.name)
+    def getObjectNameList(self):
+        return bpy.data.objects.keys()
 
-        object_list = []
-        return object_list
+    def getCollectionNameList(self):
+        return bpy.data.collections.keys()
+
+    def getCollectionList(self):
+        return bpy.data.collections
+
+    def getCollectionObjectList(self, collection_name):
+        collection_name_list = self.getCollectionNameList()
+
+        if collection_name not in collection_name_list:
+            print('[ERROR][ObjectManager::getCollectionObjectList]')
+            print('\t collection [' + collection_name + '] not found!')
+            return None
+
+        return bpy.data.collections[collection_name].objects
+
+    def getCollectionObjectNameList(self, collection_name):
+        collection_object_list = self.getCollectionObjectList(collection_name)
+
+        if collection_object_list is None:
+            print('[ERROR][ObjectManager::getCollectionObjectNameList]')
+            print('\t getCollectionObjectList failed!')
+            return None
+
+        return collection_object_list.keys()
