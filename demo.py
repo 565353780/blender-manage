99ccd660a22a8aad0b2b05ad6f35181db60e3125
chLi <565353780@qq.com>
2023-05-01 19:58:26 +0800

    add render manager demo script

--- demo.py
+++ demo.py
@@ -7,7 +7,9 @@ sys.path.append('D:/github/blender-manage')
 
 from blender_manage.Demo.object_manager import demo as demo_manage_object
 from blender_manage.Demo.shading_manager import demo as demo_manage_shading
+from blender_manage.Demo.render_manager import demo as demo_manage_render
 
 if __name__ == '__main__':
     #  demo_manage_object()
-    demo_manage_shading()
+    #  demo_manage_shading()
+    demo_manage_render()
