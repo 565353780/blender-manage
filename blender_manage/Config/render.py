c316420a3c379f5ecec278bd568328221b2603c5
chLi <565353780@qq.com>
2023-05-02 18:10:02 +0800

    create render config and camera name list config

---
+++ blender_manage/Config/render.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from blender_manage.Config.collection import COLLECTION_NAME_LIST
+
+RENDER_NAME_LIST = COLLECTION_NAME_LIST
+
+CAMERA_NAME_LIST = [
+    'TopView',
+    'Detail-1',
+    'Detail-2',
+    'Detail-3',
+]
