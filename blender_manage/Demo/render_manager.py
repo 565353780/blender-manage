a836817f7636236e1bf950ce36def59205af1882
chLi <565353780@qq.com>
2023-05-07 19:41:56 +0800

    add save folder path param to save at custom path

--- blender_manage/Demo/render_manager.py
+++ blender_manage/Demo/render_manager.py
@@ -5,8 +5,12 @@ from blender_manage.Module.render_manager import RenderManager
 
 def demo():
     color_map_name = 'morandi'
+    save_folder_path = 'D:/github/blender-manage/output/'
 
     render_manager = RenderManager()
 
-    render_manager.renderAllViews()
+    render_manager.renderAllViews(color_map_name=color_map_name, save_folder_path=save_folder_path)
     return True
+
+if __name__ == '__main__':
+    demo()
