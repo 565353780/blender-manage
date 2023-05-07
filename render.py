1664815e0f6d0e246942e08c55810cc15c73a949
chLi <565353780@qq.com>
2023-05-07 19:56:17 +0800

    add path import

--- render.py
+++ render.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+blender_manage_folder_path = 'D:/github/blender-manage'
+
+import sys
+
+sys.path.append(blender_manage_folder_path)
+
 from blender_manage.Module.render_manager import RenderManager
+
 
 def demo():
     color_map_name = 'morandi'
-    save_folder_path = 'D:/github/blender-manage/output/'
+    save_folder_path = blender_manage_folder_path + '/output/'
 
     render_manager = RenderManager()
 
