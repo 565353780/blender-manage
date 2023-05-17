04525f405be72665848b3e42b29744fadd4e1963
chLi <565353780@qq.com>
2023-05-17 23:32:45 +0800

    update pointcloud manager demo scrip

--- blender_manage/Demo/pointcloud_manager.py
+++ blender_manage/Demo/pointcloud_manager.py
@@ -4,10 +4,15 @@
 from blender_manage.Module.pointcloud_manager import PointCloudManager
 
 def demo():
-    root_folder_path = 'D:/Project_chLi/Scan2CAD/Paper Writing/figure materials/all-sim-results/scene0653_00/Scanned Scene/'
-    method_name_list = ['OurCAD+Guo', 'OurCAD+Schmid', 'ROCA+OurNBV', 'Ours']
+    root_folder_path = 'D:/Project_chLi/Scan2CAD/Paper Writing/figure materials/all-sim-results/scene0603_01/'
+    method_name_list = ['scan-1', 'scan-explicit', 'scan-ours']
     color_name = 'Col'
 
     pointcloud_manager = PointCloudManager()
+    for method_name in method_name_list:
+        ply_file_path = root_folder_path + method_name + '.ply'
+        pointcloud_manager.createColor(ply_file_path, method_name, color_name)
+    return
+
     pointcloud_manager.createColorsForMethods(root_folder_path, method_name_list, color_name)
     return True
