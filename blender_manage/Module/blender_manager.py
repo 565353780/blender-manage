652ae0ebfd5e74c944cd37b870e33c1e060ebc1f
chLi <565353780@qq.com>
2023-05-17 23:27:48 +0800

    add empty blender manager module to manage everything

---
+++ blender_manage/Module/blender_manager.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from blender_manage.Module.object_manager import ObjectManager
+from blender_manage.Module.shading_manager import ShadingManager
+from blender_manage.Module.render_manager import RenderManager
+from blender_manage.Module.pointcloud_manager import PointCloudManager
+
+
+class BlenderManager(object):
+    def __init__(self):
+        return
