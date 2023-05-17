5ad1714d67a5a59771f1f4c9d1b280c21f0b3f23
chLi <565353780@qq.com>
2023-05-17 23:54:19 +0800

    add real macaron color map

--- blender_manage/Config/color.py
+++ blender_manage/Config/color.py
@@ -3,7 +3,7 @@
 
 from blender_manage.Method.color import getColorMap
 
-macaron_color_list = [
+old_macaron_color_list = [
     "#FBB5AF", "#FBE06F", "#B0E586", "#8AD4D5", "#718DD5", "#A38DDE",
     "#9ED68C", "#61abff", "#ffb056", '#A9CBFF', "#7a7579", "#a59b95",
     "#dcd7d9", "#cb9c7a", "#8a7b93", "#dcd2bd", "#755953", "#efedeb",
@@ -12,6 +12,15 @@ macaron_color_list = [
     "#fffbe8", "#cadabd", "#e7d8d4", "#e8e8dc", "#bdbab2", "#d9d6d9",
     "#f1dfde", "#e3dca3", "#dcd2bd", "#fbeede", "#a1b2cc", "#a59b95",
     "#f18772", "#6c696a", "#a54b43", "#d8d0be", "#b0886a", "#cbcbc9", "#686954"
+]
+
+macaron_color_list = [
+    '#f1707d', '#f155369', '#ef5767', '#ae716e', '#cb8e85', '#cf8878', '#cf8878',
+    '#f1ccb8', '#f2debd', '#b8d38f', '#ddff95', '#ff9b6a', '#f1b8f1', '#d9b8f1',
+    '#f1ccb8', '#f1f1b8', '#b8f1ed', '#b8f1cc', '#e7dbca', '#e26538', '#f3d751',
+    '#fd803a', '#fe997b', '#c490a0', '#f28a63', '#df7a30', '#e96d29', '#cb7799',
+    '#e3a04f', '#edc02f', '#fecf45', '#f9b747', '#c28164', '#ed987b', '#ffe647',
+    '#e49f5e', '#ff8444', '#ac5e74', '#f0b735', '#d08a8a'
 ]
 
 morandi_color_list = [
@@ -24,6 +33,7 @@ morandi_color_list = [
 ]
 
 COLOR_MAP_DICT = {
+    'old_macaron': getColorMap(old_macaron_color_list),
     'macaron': getColorMap(macaron_color_list),
     'morandi': getColorMap(morandi_color_list),
 }
