b6ae10fc22e403597115968fa60bfe0ad7f24454
chLi <565353780@qq.com>
2025-01-18 03:48:44 +0800

    create render script on gpu server

--- render_objaverse_folders.py
+++ render_objaverse_folders.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 from time import sleep
 
@@ -5,6 +6,22 @@ from blender_manage.Module.blender_renderer import BlenderRenderer
 
 
 if __name__ == "__main__":
+    dataset_folder_path_list = [
+        '/home/chli/chLi/Dataset/Objaverse_82K/glbs/',
+        '/mnt/d/chLi/Dataset/Objaverse_82K/glbs/',
+        '/mnt/data/jintian/chLi/Dataset/Objaverse_82K/glbs/',
+    ]
+
+    shape_folder_path = None
+    for dataset_folder_path in dataset_folder_path_list:
+        if os.path.exists(dataset_folder_path):
+            shape_folder_path = dataset_folder_path
+            break
+
+    if shape_folder_path is None:
+        print('dataset not found!')
+        exit()
+
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--gpu_id",
@@ -14,8 +31,9 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    shape_folder_path = '/home/chli/chLi/Dataset/Objaverse_82K/glbs/'
-    save_image_folder_path = '/home/chli/chLi/Dataset/Objaverse_82K/render_jpg_v2/'
+    # shape_folder_path = '/home/chli/chLi/Dataset/Objaverse_82K/glbs/'
+    render_image_num = 12
+    save_image_folder_path = shape_folder_path.replace('/glbs/', '/render_jpg_v2/')
     use_gpu = True
     overwrite = False
     is_background = True
@@ -25,8 +43,9 @@ if __name__ == "__main__":
 
     while True:
         assert BlenderRenderer.isValid()
-        process = BlenderRenderer.renderFolders(
+        process = BlenderRenderer.renderAroundObjaverseFolders(
             shape_folder_path,
+            render_image_num,
             save_image_folder_path,
             use_gpu,
             overwrite,
