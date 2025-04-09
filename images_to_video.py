1dd331f0fe58041716d30c56a47c95f9df76b848
chLi <565353780@qq.com>
2025-04-10 02:23:11 +0800

    split functions

--- images_to_video.py
+++ images_to_video.py
@@ -1,21 +1,29 @@
 from blender_manage.Method.video import toVideo, toGif
 
 
-if __name__ == '__main__':
-    toVideo(
-        '/home/chli/chLi/Results/ma-sh/output/fit_render/fixed/bunny/anchor-50_1000x1000/pcd/',
-        '/home/chli/chLi/Results/ma-sh/output/video/bunny_fit_1000x1000.mp4',
-        fps=30,
-        repeat_tag_list=[1],
-        overwrite=False,
-    )
-    toVideo(
-        '/home/chli/chLi/Results/ma-sh/output/fit_error_mesh_render/bunny/anchor-50_1000x1000/mash/',
-        '/home/chli/chLi/Results/ma-sh/output/video/bunny_fit_error_1000x1000.mp4',
-        fps=30,
-        repeat_tag_list=[1],
-        overwrite=False,
-    )
+def toBunnyFitVideo():
+    for anchor_num in [10, 20, 50, 100, 200, 400]:
+        toVideo(
+            '/home/chli/chLi/Results/ma-sh/output/render/bunny-' + str(anchor_num) + 'anc_1000x1000/',
+            '/home/chli/chLi/Results/ma-sh/output/video/bunny-' + str(anchor_num) + 'anc_1000x1000.mp4',
+            fps=90,
+            repeat_tag_list=[1],
+            overwrite=True,
+        )
+    return
+
+def toBunnyFitErrorVideo():
+    for anchor_num in [10, 20, 50, 100, 200, 400]:
+        toVideo(
+            '/home/chli/chLi/Results/ma-sh/output/fit_error_mesh_render/bunny/anchor-' + str(anchor_num) + '_1000x1000/mash/',
+            '/home/chli/chLi/Results/ma-sh/output/video/bunny_' + str(anchor_num) + 'anc_error_1000x1000.mp4',
+            fps=90,
+            repeat_tag_list=[1],
+            overwrite=True,
+        )
+    return
+
+def toCropVideo():
     toVideo(
         '/home/chli/chLi/Results/ma-sh/output/render_crop/XiaomiSU7_1000x1000/anc-1500/',
         '/home/chli/chLi/Results/ma-sh/output/video/XiaomiSU7_crop_1000x1000.mp4',
@@ -30,3 +38,28 @@ if __name__ == '__main__':
         repeat_tag_list=[-1, 1],
         overwrite=True,
     )
+    return
+
+def toThingi10KGif():
+    toGif(
+        '/home/chli/chLi/Results/3DShape2VecSet/render/Thingi10K-46602_1000x1000/Thingi10K-46602/',
+        '/home/chli/chLi/Results/3DShape2VecSet/video/Thingi10K-46602_1000x1000.gif',
+        fps=10,
+        repeat_tag_list=[1],
+        overwrite=True,
+    )
+    toGif(
+        '/home/chli/chLi/Results/3DShape2VecSet/render/Thingi10K-61258_1000x1000/Thingi10K-61258/',
+        '/home/chli/chLi/Results/3DShape2VecSet/video/Thingi10K-61258_1000x1000.gif',
+        fps=10,
+        repeat_tag_list=[1],
+        overwrite=True,
+    )
+
+    return
+
+if __name__ == '__main__':
+    # toBunnyFitVideo()
+    # toBunnyFitErrorVideo()
+    # toCropVideo()
+    toThingi10KGif()
