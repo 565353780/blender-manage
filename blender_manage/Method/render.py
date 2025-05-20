441d7a61b561b120dc99de0780d9b42858c64d43
chLi <565353780@qq.com>
2025-05-20 15:57:53 +0800

    add vae eval data render

--- blender_manage/Method/render.py
+++ blender_manage/Method/render.py
@@ -11,18 +11,18 @@ def renderFile(
     overwrite: bool = False,
     early_stop: bool = False,
 ) -> bool:
-    image_format = 'png'
+    image_format = "png"
 
     if not isFileTypeValid(shape_file_path):
-        print('[ERROR][render::renderFile]')
-        print('\t shape file not valid!')
-        print('\t shape_file_path:', shape_file_path)
+        print("[ERROR][render::renderFile]")
+        print("\t shape file not valid!")
+        print("\t shape_file_path:", shape_file_path)
         return False
 
-    object_name = shape_file_path.split('/')[-1].split('.')[0]
+    object_name = shape_file_path.split("/")[-1].split(".")[0]
 
-    if save_image_file_basepath[-1] == '/':
-        save_image_file_basepath += object_name + '.' + image_format
+    if save_image_file_basepath[-1] == "/":
+        save_image_file_basepath += object_name + "." + image_format
 
     if os.path.exists(save_image_file_basepath):
         if not overwrite:
@@ -35,65 +35,73 @@ def renderFile(
     blender_manager.removeAll()
 
     blender_manager.setRenderer(
-        resolution=[518, 518],
-        engine_name='CYCLES',
-        use_gpu=use_gpu)
+        resolution=[1000, 1000], engine_name="CYCLES", use_gpu=use_gpu
+    )
 
     blender_manager.createLight(
-        name='light_top',
-        light_type='AREA',
-        collection_name='Lights',
+        name="light_top",
+        light_type="AREA",
+        collection_name="Lights",
         position=[0, 0, 2],
         rotation_euler=[0, 0, 0],
         energy=50,
-        size=5)
+        size=5,
+    )
     blender_manager.createLight(
-        name='light_front',
-        light_type='AREA',
-        collection_name='Lights',
+        name="light_front",
+        light_type="AREA",
+        collection_name="Lights",
         position=[0, 2, 0],
         rotation_euler=[-90, 0, 0],
         energy=50,
-        size=5)
+        size=5,
+    )
     # blender_manager.setCollectionVisible('Lights', False)
 
     blender_manager.createCamera(
-        name='camera_1',
-        camera_type='PERSP',
-        collection_name='Cameras',
+        name="camera_1",
+        camera_type="PERSP",
+        collection_name="Cameras",
         position=[-1.0581, 1.7608, 0.83803],
-        rotation_euler=[65.161, 0, -148.51])
+        rotation_euler=[65.161, 0, -148.51],
+    )
     # blender_manager.setCollectionVisible('Cameras', False)
 
-    camera_name_list = blender_manager.object_manager.getCollectionObjectNameList('Cameras')
+    camera_name_list = blender_manager.object_manager.getCollectionObjectNameList(
+        "Cameras"
+    )
 
-    collection_name = 'shapes'
+    collection_name = "shapes"
 
     blender_manager.loadObject(
         shape_file_path=shape_file_path,
         name=object_name,
         collection_name=collection_name,
-        #rotation_euler=[-94, 26, 126], # bunny
-        #rotation_euler=[2, -2, -18], # XiaomiSU7
-        #rotation_euler=[94, 0, 108], # RobotArm
-        #rotation_euler=[90, 0, 148], # Washer
-        #rotation_euler=[25.208, -2.5156, -155.94], # KITTI mash
-        rotation_euler=[-18.126, -4.5996, -8.7924], # KITTI others
-        scale=[0.008, 0.008, 0.008], # KITTI
+        # rotation_euler=[-94, 26, 126], # bunny
+        # rotation_euler=[2, -2, -18], # XiaomiSU7
+        # rotation_euler=[94, 0, 108], # RobotArm
+        # rotation_euler=[90, 0, 148], # Washer
+        # rotation_euler=[25.208, -2.5156, -155.94], # KITTI mash
+        # rotation_euler=[-18.126, -4.5996, -8.7924],  # KITTI others
+        # scale=[0.008, 0.008, 0.008],  # KITTI
     )
 
-    if 'LN3Diff' in shape_file_path:
+    # blender_manager.object_manager.normalizeObject(object_name)
+
+    if "LN3Diff" in shape_file_path:
         blender_manager.object_manager.setObjectRotationEuler(object_name, [180, 0, 0])
 
     if not blender_manager.shading_manager.useObjectColor(object_name):
-        blender_manager.shading_manager.paintColorMapForObject(object_name, 'pcd')
+        blender_manager.shading_manager.paintColorMapForObject(object_name, "pcd")
 
-        if 'pcd' in object_name or 'xyz' in object_name:
-            blender_manager.pointcloud_manager.createColor(object_name, 0.004, 'pcd_0', object_name)
+        if "pcd" in object_name or "xyz" in object_name:
+            blender_manager.pointcloud_manager.createColor(
+                object_name, 0.004, "pcd_0", object_name
+            )
 
-    #FIXME: to force set color for compare with other methods only
+    # FIXME: to force set color for compare with other methods only
     # user can remove this line to auto load object colors
-    blender_manager.shading_manager.paintColorMapForObject(object_name, 'pcd')
+    blender_manager.shading_manager.paintColorMapForObject(object_name, "pcd")
 
     # blender_manager.setCollectionVisible(collection_name, False)
     # blender_manager.setCollectionRenderable(collection_name, False)
@@ -101,7 +109,9 @@ def renderFile(
     blender_manager.setObjectRenderable(object_name, True)
 
     if early_stop:
-        camera_name_list = blender_manager.object_manager.getCollectionObjectNameList('Cameras')
+        camera_name_list = blender_manager.object_manager.getCollectionObjectNameList(
+            "Cameras"
+        )
         camera_name = camera_name_list[0]
 
         blender_manager.camera_manager.changeToCameraView(camera_name)
--- render_folders.py
+++ render_folders.py
@@ -3,31 +3,47 @@ from time import sleep
 from blender_manage.Module.blender_renderer import BlenderRenderer
 
 
+shape_data_dict = {
+    "RobotArm": [
+        "/home/chli/chLi/Results/ma-sh/output/fit/fixed/RobotArm/",
+        "/home/chli/chLi/Results/ma-sh/output/fit_render/fixed/RobotArm/",
+    ],
+    "Thingi10K-46602": [
+        "/home/chli/chLi/Results/render/Thingi10K/46602/",
+        "/home/chli/chLi/Results/render/Thingi10K/render/46602/",
+    ],
+    "Thingi10K-61258": [
+        "/home/chli/chLi/Results/render/Thingi10K/61258/",
+        "/home/chli/chLi/Results/render/Thingi10K/render/61258/",
+    ],
+    "KITTI": [
+        "/home/chli/chLi/Results/render/KITTI/",
+        "/home/chli/chLi/Results/render/KITTI_render/",
+    ],
+    "Bunny-50anc": [
+        "/home/chli/chLi/Results/ma-sh/output/fit_error_mesh/bunny/anchor-50/",
+        "/home/chli/chLi/Results/ma-sh/output/fit_error_mesh_render/bunny/anchor-50_1000x1000/",
+    ],
+    "XiaomiSU7": [
+        "/home/chli/chLi/Results/ma-sh/output/crop/XiaomiSU7/",
+        "/home/chli/chLi/Results/ma-sh/output/render_crop/XiaomiSU7_1000x1000/",
+    ],
+    "Washer": [
+        "/home/chli/chLi/Results/ma-sh/output/crop/Washer/",
+        "/home/chli/chLi/Results/ma-sh/output/render_crop/Washer_1000x1000/",
+    ],
+    "vae-eval": [
+        "/home/chli/chLi/Dataset/vae-eval/render_pcd/",
+        "/home/chli/chLi/Dataset/vae-eval/manifold_mash_render-4000anc/",
+    ],
+}
+
 if __name__ == "__main__":
     assert BlenderRenderer.isValid()
 
     from custom_path import data_dict, s2v_gen_shape_id_list as shape_id_list
 
-    shape_folder_path = '/home/chli/chLi/Results/ma-sh/output/fit/fixed/RobotArm/'
-    save_image_folder_path = '/home/chli/chLi/Results/ma-sh/output/fit_render/fixed/RobotArm/'
-
-    shape_folder_path = '/home/chli/chLi/Results/render/Thingi10K/46602/'
-    save_image_folder_path = '/home/chli/chLi/Results/render/Thingi10K/render/46602/'
-
-    shape_folder_path = '/home/chli/chLi/Results/render/Thingi10K/61258/'
-    save_image_folder_path = '/home/chli/chLi/Results/render/Thingi10K/render/61258/'
-
-    shape_folder_path = '/home/chli/chLi/Results/render/KITTI/'
-    save_image_folder_path = '/home/chli/chLi/Results/render/KITTI_render/'
-
-    #shape_folder_path = '/home/chli/chLi/Results/ma-sh/output/fit_error_mesh/bunny/anchor-50/'
-    #save_image_folder_path = '/home/chli/chLi/Results/ma-sh/output/fit_error_mesh_render/bunny/anchor-50_1000x1000/'
-
-    #shape_folder_path = '/home/chli/chLi/Results/ma-sh/output/crop/XiaomiSU7/'
-    #save_image_folder_path = '/home/chli/chLi/Results/ma-sh/output/render_crop/XiaomiSU7_1000x1000/'
-
-    #shape_folder_path = '/home/chli/chLi/Results/ma-sh/output/crop/Washer/'
-    #save_image_folder_path = '/home/chli/chLi/Results/ma-sh/output/render_crop/Washer_1000x1000/'
+    shape_folder_path, save_image_folder_path = shape_data_dict["vae-eval"]
 
     workers_per_cpu = 1
     workers_per_gpu = 6
